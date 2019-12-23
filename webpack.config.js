e71e28eace56aeeada461b9bf5f564d38556bdad
Eileen Duffner <eileen.duffner@gmail.com>
2019-12-22 22:49:55 -0600

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+// webpack.config.js
+const path = require('path')
+
+module.exports = {
+  entry: './app/index.js',
+  module: {
+    rules: [
+      { test: /\.svg$/, use: 'svg-inline-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+      { test: /\.(js)$/, use: 'babel-loader' }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  plugins: []
+}
